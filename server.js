dd68904a523502ea31df388f1f8610f37c1456ec
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2023-06-06 13:40:33 +0600

    Fix server.js

--- server.js
+++ server.js
@@ -21,20 +21,18 @@ app.use(express.json());
 // API routes
 app.use('/api/user', userRoutes);
 
-// Serve static files from the 'public' directory
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.send('Server is running.');
+});
 
-// Error handler middleware
+// Middleware
 app.use(notFound);
 app.use(errorHandler);
 
-// Route handler for the root path ("/")
-app.get('/', (req, res) => {
-  res.send('Hello, world! This is the root path.');
-});
-
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port http://localhost:${PORT}`,
+  ),
+);
