58096a62cbeccdc391b37bb143b9917b7f6115dd
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2023-06-06 13:33:35 +0600

    Fix server.js

--- server.js
+++ server.js
@@ -29,6 +29,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(notFound);
 app.use(errorHandler);
 
+// Route handler for the root path ("/")
+app.get('/', (req, res) => {
+  res.send('Hello, world! This is the root path.');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
